f8737eb2cab07d2bbea55d2a6ef6162fea892e30
Igor-Kuz <dragondusty@gmail.com>
2021-10-10 23:08:45 +0300

    the new one commit

--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
 
+
 def home(request):
 	return render(request, 'home.html', {})
+
 
 def contact(request):
 	if request.method == "POST":
@@ -10,12 +12,11 @@ def contact(request):
 		message_email = request.POST['message-email']
 		message = request.POST['message']
 
-		# send an email
 		send_mail(
-			message_name, # subject
-			message, # message
-			message_email, # from email
-			['john@codemy.com'], # To Email
+			message_name,
+			message,
+			message_email,
+			['your@email.com'],
 			)
 
 		return render(request, 'contact.html', {'message_name': message_name})
@@ -27,8 +28,10 @@ def contact(request):
 def about(request):
 	return render(request, 'about.html', {})
 
+
 def pricing(request):
 	return render(request, 'pricing.html', {})
+
 
 def service(request):
 	return render(request, 'service.html', {})
@@ -45,13 +48,14 @@ def appointment(request):
 		your_message = request.POST['your-message']
 		
 		# send an email
-		appointment = "Name: " + your_name + " Phone: " + your_phone + " Email: " + your_email + " Address: " + your_address + " Schedule: " + your_schedule + " Day: " + your_date + " Message: " + your_message
+		appointment = "Name: " + your_name + " Phone: " + your_phone + " Email: " + your_email + " Address: "\
+			+ your_address + " Schedule: " + your_schedule + " Day: " + your_date + " Message: " + your_message
 
 		send_mail(
-			'Appointment Request', # subject
-			appointment, # message
-			your_email, # from email
-			['drIgorK@gmail.com'], # To Email
+			'Appointment Request',
+			appointment,
+			your_email,
+			['drIgorK@gmail.com'],
 			)
 		
 		return render(request, 'appointment.html', {
@@ -66,71 +70,3 @@ def appointment(request):
 
 	else:
 		return render(request, 'home.html', {})
-
-'''
-
-
-def contact(request):
-	if request.method == "POST":
-		message_name = request.POST['message-name']
-		message_email = request.POST['message-email']
-		message = request.POST['message']
-
-		#send an email
-		send_mail(
-			message_name, # subject
-			message, #message itself
-			message_email, # from email
-			['somename@gmail.com'], # to email
-			)
-
-		return render(request, 'contact.html', {'message_name': message_name}) #возвращаем назад на ту же страницу после заполнения форм
-
-
-	else: 
-		return render(request, 'contact.html', {})
-
-
-def appointment(request):
-	if request.method == "POST":
-		your_name = request.POST['your-name']
-		your_phone = request.POST['your-phone']
-		your_email = request.POST['your-email']
-		your_address = request.POST['your-address']
-		your_scheldule = request.POST['your-scheldule']
-		your_date = request.POST['your-date']
-		your_message = request.POST['your-message']
-
-		
-
-		#send an email
-
-		appointment = "Name: " + your_name + " Phone: " + your_phone + " Email: " + your_email + " Address: " + your_address + " Scheldule: " + your_scheldule + " Day: " + your_date + " Message: " + your_message
-
-
-
-		# appointment = " Name: " + your_name + "Phone" + your_phone + "Email:"+ your_email + "Address: " + your_address + "Scheldule" + your_scheldule + "Day" + your_date + "Message" + your_message
-
-		send_mail(
-			'Appointment Request', # subject
-			appointment, #message itself
-			your_email, # from email
-			['somename@gmail.com'], # to email
-			)	
-		
-		return render(request, 'appointment.html', {
-			'your_name': your_name,
-			'your_phone': your_phone,
-			'your_email': your_email,
-			'your_address': your_address,
-			'your_scheldule': your_scheldule,
-			'your_date': your_date,
-			'your_message': your_message
-
-			})  
-		#возвращаем назад на ту же страницу после заполнения форм
-
-
-	else: 
-		return render(request, 'index.html', {})	
-		'''
